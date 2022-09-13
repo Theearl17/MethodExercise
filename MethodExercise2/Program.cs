93d8ea84c2d0184a5d801d800807367de993f9e6
earl starks <earlstarks@rocketmail.com>
2022-09-13 10:32:49 -0500

    Method Exercise 2

--- MethodExercise2/Program.cs
+++ MethodExercise2/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("What is your first name?");// input 
+﻿using System.ComponentModel;
+
+Console.WriteLine("What is your first name?");// input 
 var userName = Console.ReadLine();
 
 Console.WriteLine($"Hi, {userName}. What is your favorite color?"); // input
@@ -10,12 +12,32 @@ var animal = Console.ReadLine();
 Console.WriteLine($"{animal} is the best! What is your favorite band?");// input
 var band = Console.ReadLine();
 
-Console.WriteLine($"{band} is sketch as well?"); //input
+Console.WriteLine($"{band} is well-known. He was a likeable guy whose passing was related to his struggle with existentialism. You are wise, Earl."); //input
 
-//// Exercise II / Mathematica
-//{
-//    Sum(5, 5) // would return 10    
-//    Sum(1, 3, 5) // would return 19   
-//    Sum(0, 1, 2, 3)// would return 6
-//}
+
+// Exercise II / Mathematica
+
+Console.WriteLine(Add(5,5));
+Console.WriteLine(Divide(2,2));
+Console.WriteLine(Multiply(3,3));
+Console.WriteLine(Subtract(4,4));
+
+
+    static int Add(int num1, int num2)
+    { 
+        return num1 + num2;
+    }
+    static int Divide(int num3, int num4)
+    {
+        return num3 / num4;  
+    }
+    static int Multiply(int num5, int num6)
+    {
+        return num5 * num6;
+    }
+    static int Subtract(int num7, int num8)
+    {
+        return num7 - num8;
+    }
+
 
