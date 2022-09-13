44da4a28bf8e4d8cdc464514d61e9e93ab1044e8
earl starks <earlstarks@rocketmail.com>
2022-09-13 09:36:43 -0500

    Method Exercise

---
+++ MethodExercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("What is your first name?");// input 
+var userName = Console.ReadLine();
+
+Console.WriteLine($"Hi, {userName}. What is your favorite color?"); // input
+var color = Console.ReadLine();
+
+Console.WriteLine($"{color} is sketch. What is your favorite animal?");// input
+var animal = Console.ReadLine();
+
+Console.WriteLine($"{animal} is the best! What is your favorite band?");// input
+var band = Console.ReadLine();
+
+Console.WriteLine($"{band} is sketch as well?"); //input
+
+//// Exercise II / Mathematica
+//{
+//    Sum(5, 5) // would return 10    
+//    Sum(1, 3, 5) // would return 19   
+//    Sum(0, 1, 2, 3)// would return 6
+//}
+
